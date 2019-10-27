2fb23f32aae541b545214f6705e650e101c86173
Eliane-5 <irankundaeliane9@gmail.com>
2019-10-28 00:17:56 +0200

    adding logout functionality

--- app/src/main/java/com/moringaschool/craftypictures/ChoiceActivity.java
+++ app/src/main/java/com/moringaschool/craftypictures/ChoiceActivity.java
@@ -7,10 +7,15 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +28,31 @@ public class ChoiceActivity extends AppCompatActivity {
     @BindView(R.id.blackAndWhite) Button mBlackAndWhiteButton;
     @BindView(R.id.colorful) Button mColorfulButton;
     @BindView(R.id.exhibition) Button mExhibitButton;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(ChoiceActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
